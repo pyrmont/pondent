ca1318b44879177f84b904425da9b50e2012a06e
Michael Camilleri <mike@inqk.net>
2020-02-23 10:35:36 +0900

    Support backticks and count URL length in links

--- src/pondent/markdown.cljs
+++ src/pondent/markdown.cljs
@@ -7,13 +7,23 @@
   "Return true if the two arguments represent enclosing Markdown characters."
   [open close]
   (cond
-    (nil? open) false
+    (nil? open)   false
     (= "**" open) (= "**" close)
     (= "__" open) (= "__" close)
-    (= "*" open) (= "*" close)
-    (= "_" open) (= "_" close)
-    (= "[" open) (str/ends-with? close ")")
+    (= "*" open)  (= "*" close)
+    (= "_" open)  (= "_" close)
+    (= "`" open)  (= "`" close)
+    (= "[" open)  (str/ends-with? close ")")
     :else false))
+
+(defn meta-length
+  "Return the number of characters to be counted."
+  [open close]
+  (if (not (meta-match? open close))
+    nil
+    (if (str/starts-with? close "](")
+      (- (count close) 4) ;; 4 for the [, ], ( and )
+      0)))
 
 (defn next-plain-char
   "Return the position of the next unambiguously plain character in `text`
@@ -25,6 +35,7 @@
       (str/starts-with? remainder "__") (+ 2 start)
       (str/starts-with? remainder "*")  (+ 1 start)
       (str/starts-with? remainder "_")  (+ 1 start)
+      (str/starts-with? remainder "`")  (+ 1 start)
       (str/starts-with? remainder "[")  (+ 1 start)
       (str/starts-with? remainder "](") (if-let [next-pos (str/index-of text ")" start)]
                                           (+ 1 next-pos)
@@ -41,8 +52,8 @@
           (if (= i next-pos) ;; The current character is a plain character
             (recur (inc i) (inc length) meta-stack)
             (let [meta-chars (subs text i next-pos)]
-              (if (meta-match? (peek meta-stack) meta-chars)
-                (recur next-pos length (pop meta-stack))
+              (if-let [extra-length (meta-length (peek meta-stack) meta-chars)]
+                (recur next-pos (+ extra-length length) (pop meta-stack))
                 (recur next-pos length (conj meta-stack meta-chars))))))
         (+ length (reduce #(+ %1 (count %2)) 0 meta-stack))))))
 
