1d895e2a37d2c91f1611e1233d7f211c3ec74753
Michael Camilleri <mike@inqk.net>
2020-03-08 08:37:05 +0900

    Add additional line breaks to pondent.posting

--- src/pondent/posting.cljs
+++ src/pondent/posting.cljs
@@ -5,15 +5,18 @@
             [pondent.time :as time]
             [promesa.core :as p]))
 
+
 (defn format-date
   "Format `date` into a form appropriate for frontmatter."
   [date]
   (str "date: " (time/date->str date "yyyy-MM-dd HH:mm:ss Z\n")))
 
+
 (defn format-title
   "Format `title` into a form appropriate for frontmatter."
   [title]
-  (when (not (string/blank? title))  (str "title: \"" title "\"\n")))
+  (when (not (string/blank? title)) (str "title: \"" title "\"\n")))
+
 
 (defn format-categories
   "Format `categories` into a form appropriate for frontmatter."
@@ -25,10 +28,12 @@
               (string/join ", "))
          "]\n")))
 
+
 (defn format-body
   "Format `body` into a form appropriate for frontmatter."
   [body]
   (str (string/trim-newline body) "\n"))
+
 
 (defn format-post
   "Format a post for Jekyll."
@@ -40,6 +45,7 @@
        "---\n\n"
        (format-body body)))
 
+
 (defn input-errors
   "Return any errors in the input."
   [{:keys [content slug]}]
@@ -47,15 +53,18 @@
     (string/blank? content) {:kind :missing-content}
     (string/blank? slug)    {:kind :missing-slug}))
 
+
 (defn post-path
   "Construct a path to the post."
   [dir date slug]
   (str dir (time/date->str date "yyyy-MM-dd") "-" slug ".md"))
 
+
 (defn success?
   "Returns `true` if `result` represents success."
   [result]
   (and (map? result) (= :success (:kind result))))
+
 
 (defn create-post
   "Create the `post` using the `settings`."
