1a7d35467542edf08e0b1c46d4ab47843b2318cb
Michael Camilleri <mike@inqk.net>
2020-03-18 15:47:06 +0900

    Add specs to pondent.time

--- src/pondent/time.cljs
+++ src/pondent/time.cljs
@@ -1,6 +1,18 @@
 (ns pondent.time
-  (:require [goog.date.DateTime]
+  (:require [clojure.spec.alpha :as s]
+            [goog.date.DateTime]
             [goog.i18n.DateTimeFormat]))
+
+
+;; Specs
+
+(def iso-8601 #"/d{4}-[0-1]/d-[0-3]/dT[0-2]/d:[0-5]/d:[0-5]/d[+-][0-2]/d:[0-5]/d")
+
+(s/def ::date #(instance? goog.date.DateTime %))
+(s/def ::date-pattern string?)
+(s/def ::date-iso-8601 (s/and string? #(re-matches iso-8601 %)))
+(s/def ::time-offset (s/double-in :min -24.0 :max 24.0 :NaN? false :infinite? false))
+
 
 ;; Date conversions
 
@@ -12,21 +24,33 @@
   ([date patt]
    (.format (new goog.i18n.DateTimeFormat patt) date)))
 
+(s/fdef date->str
+  :args (s/cat :date ::date :patt (s/? ::date-pattern))
+  :ret  string?)
+
+
 (defn now
   "Returns the current time."
   []
   (goog.date.DateTime.))
 
+(s/fdef now
+  :ret ::date)
+
+
 (defn str->date
   "Parse a date, `s`, in ISO 8601 format and return a goog.date.DateTime object.
   An hour `offset` can also be provided that will be used to adjust the final
-  object."
+  object. If `s` cannot be parsed as a valid time, return `nil`."
   ([s]
    (str->date s 0))
   ([s offset]
    (let [millis (.parse js/Date s)]
-     (if (.isNaN js/Number millis)
-       nil
+     (when-not (.isNaN js/Number millis)
        (->> millis
             (+ (* offset 60 60 1000))
             (.fromTimestamp goog.date.DateTime))))))
+
+(s/fdef str->date
+  :args (s/cat :s ::date-iso-8601 :offset (s/? ::time-offset))
+  :ret  (s/nilable ::date))
