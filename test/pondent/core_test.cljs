a2f2ae5942d90f3cc57a9bfe7f6af9f2ba1ccd1a
Michael Camilleri <mike@inqk.net>
2020-03-18 15:45:44 +0900

    Remove testing infrastructure

--- test/pondent/core_test.cljs
+++
@@ -1,3 +0,0 @@
-(ns pondent.core-test
-    (:require
-     [cljs.test :refer-macros [deftest is testing]]))
--- test/pondent/test_runner.cljs
+++
@@ -1,9 +0,0 @@
-;; This test runner is intended to be run from the command line
-(ns pondent.test-runner
-  (:require
-    ;; require all the namespaces that you want to test
-    [pondent.core-test]
-    [figwheel.main.testing :refer [run-tests-async]]))
-
-(defn -main [& args]
-  (run-tests-async 5000))
