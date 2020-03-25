a1568dccf162e2cc50386506b97d6bd79068133c
Michael Camilleri <mike@inqk.net>
2020-03-25 18:50:32 +0900

    Add and reorganise utility functions in pondent.file

--- src/pondent/file.cljs
+++ src/pondent/file.cljs
@@ -6,11 +6,7 @@
             [promesa.core :as p]))
 
 
-(defn year
-  "Get the year `file`was uploaded."
-  [file]
-  (-> file :date (time/date->str "yyyy")))
-
+;; Independent properties
 
 (defn filename
   "Get the name of `file`."
@@ -18,20 +14,55 @@
   (-> file :object .-name))
 
 
+(defn year
+  "Get the year `file` was uploaded."
+  [file]
+  (-> file :date (time/date->str "yyyy")))
+
+
+;; Dependent properties
+
+(defn extension
+  "Get the extension of `file`. By default, the extension is transformed to
+  lower-case."
+  ([file]
+   (extension file true))
+  ([file lower-case?]
+   (let [ext (-> file filename path/extension)]
+     (if lower-case? (string/lower-case ext) ext))))
+
+
+;; Transformations
+
 (defn hashed-name
   "Get the hashed name of the file."
   [file]
   (let [base (-> file :id str gstring/hashCode)
-        extension (-> file filename path/extension)]
+        extension (-> file (extension false))]
     (str base "." extension)))
 
+
+(defn url
+  "Get the file URL."
+  [file settings]
+  (str (:uploads-url settings) (year file) "/" (hashed-name file)))
+
+
+;; Miscellaneous
 
 (defn image?
    "Check if `file` is an image."
    [file]
-   (case (-> file filename path/extension string/lower-case)
-     ("jpg" "jpeg" "png" "gif" "webp" "heif") true
-     false))
+   (contains? #{"gif" "heif" "jpg" "jpeg" "png" "webp"}
+             (-> file extension)))
+
+
+(defn movie?
+  " Check if `file` is a movie."
+  [file]
+  (contains? #{"avi" "mkv" "mov" "mp4"}
+             (-> file extension)))
+
 
 (defn read-async
   "Return a Promesa promise that reads `file`."
@@ -49,9 +80,3 @@
                         :content content}))))
         (set! (.-onerror reader) rej)
         (.readAsDataURL reader (:object file))))))
-
-
-(defn url
-  "Get the file URL."
-  [file settings]
-  (str (:uploads-url settings) (year file) "/" (hashed-name file)))
