a05e00e112cc36f6a27e5ee98079b84256210167
Michael Camilleri <mike@inqk.net>
2020-03-28 13:40:46 +0900

    Add Tailwind configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    theme: {},
+    variants: {
+      cursor: ['responsive', 'hover', 'focus', 'disabled'],
+      opacity: ['responsive', 'hover', 'focus', 'disabled']
+    },
+    plugins: [],
+};
